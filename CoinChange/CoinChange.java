c98c902c4c0838b75a53ef6fe7ac814fcf2dd1ec
Mustafa Mokashi <neyaadeez@gmail.com>
2024-02-26 02:29:29 -0800

    CoinChangeDFS

---
+++ CoinChange/CoinChange.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class CoinChange {
+    int ways = 0;
+
+    public int noWays(int[] coins, int n, int amount){
+        if(amount == 0){
+            return 1;
+        }
+
+        if(amount < 0){
+            return 0;
+        }
+
+        if(n <= 0){
+            return 0;
+        }
+
+        return noWays(coins, n, amount-coins[n-1]) + noWays(coins, n-1, amount);
+    }
+
+    public int DFS(int[] coins, int amount){
+        if(amount == 0){
+            return 0;
+        }
+        if(amount < 0)
+            return Integer.MAX_VALUE;
+
+        int count = Integer.MAX_VALUE;
+        for(int i=0; i<coins.length; i++){
+            int res = DFS(coins, amount-coins[i]);
+            if(res != Integer.MAX_VALUE){
+                count = Math.min(count, res+1);
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args){
+        CoinChange cc = new CoinChange();
+        System.out.println("Coin Change Problem");
+        System.out.println("1. DFS Approach");
+        System.out.println("2. Botton-Up DP Approach");
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Enter Number of Coins: ");
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        System.out.println("Enter Values: ");
+        for(int i=0; i<n; i++){
+            coins[i] = sc.nextInt();
+        }
+        System.out.println("Enter the Amount: ");
+        int amount = sc.nextInt();
+        sc.close();
+        int answer = 0;
+        switch(x){
+            case 1:
+                answer = cc.DFS(coins, amount);
+                System.out.println("DFS Approach Answer: "+ answer);
+                System.out.println("Number of Ways: "+ cc.noWays(coins, n, amount));
+                break;
+            case 2:
+                // answer = cc.bottomUpApproach(coins, amount);
+                // System.out.println("Botton-Up Approach Answer: "+ answer);
+                // System.out.println("Iterations: "+ cc.iterations);
+                // break;
+            default:
+                System.out.println("Wrong Choice.");
+        }
+
+    }
+}
---
+++ CoinChange/README.md
@@ -0,0 +1,52 @@
+# Coin Change Problem
+
+## Test Cases
+Test Case 1:
+```
+Coins: [1, 2, 5]
+Amount: 5
+Expected Output - Least Number of Coins: 1
+Expected Output - Number of Ways: 4
+```
+
+Test Case 2:
+```
+Coins: [1, 2, 5]
+Amount: 11
+Expected Output - Least Number of Coins: 3
+Expected Output - Number of Ways: 11
+```
+
+Test Case 3:
+```
+Coins: [2]
+Amount: 3
+Expected Output - Least Number of Coins: answer should be considered as 0 (returned value is Infinity() or 2147483647)
+Expected Output - Number of Ways: 0
+```
+
+Test Case 4:
+```
+Coins: [1, 3, 4]
+Amount: 7
+Expected Output - Least Number of Coins: 2
+Expected Output - Number of Ways: 5
+```
+
+Test Case 5:
+```
+Coins: [2, 3, 4, 5]
+Amount: 10
+Expected Output - Least Number of Coins: 2
+Expected Output - Number of Ways: 7
+```
+
+## Approach 1: DFS Approach
+
+The coin change problem is a classic algorithmic problem often solved using dynamic programming. However, it can also be solved using depth-first search (DFS) with backtracking. Here's how you can approach solving the coin change problem using DFS with backtracking:
+1. **Formulate the problem**: Given a target amount and a set of coin denominations, find the total number of ways to make up that amount using any combination of coins.
+2. **Define the DFS function**: Create a recursive function that explores all possible combinations of coins to reach the target amount.
+3. **Base case**: Define the base case for the recursion. This is when the target amount becomes 0, meaning we have found a valid combination of coins.
+4. **Recursive step**: In each recursive call, try using each coin denomination available. Recur with the updated target amount and keep track of the number of ways found.
+5. **Backtracking**: Undo the changes made during the recursive step to explore other possibilities.
+
